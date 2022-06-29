5093500b4c0581961bee2b1c3a14e39f2006801d
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2022-06-29 20:34:48 +0200

    added a jar file for jdbc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,7 @@ import  java.sql.Connection;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        /*try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace", "root", "Root_password");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from person");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("person_ID")+" " + resultSet.getString("first_name")+" "+ resultSet.getString("last_name"));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }*/
+
         Trial trial = new Trial();
         trial.trialFunction();
 
--- src/com/company/Trial.java
+++ src/com/company/Trial.java
@@ -4,10 +4,10 @@ import java.sql.*;
 class Trial {
     public  void trialFunction() {
         try {
+            // url is jdbc:mysql://localhost:3306/{database name}   user is the user of the database on the machine password should be the same as well
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace", "root", "Root_password");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from person");
-            int i =0;
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("person_ID") + resultSet.getString("first_name") + resultSet.getString("last_name"));
             }
