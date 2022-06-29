b8d71b51b7cad5ea31e3909039002e84339a4e42
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2022-06-29 04:15:50 +0200

    jdbc trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,27 @@
 package com.company;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
-
+import  java.sql.Connection;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /*try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace", "root", "Root_password");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from person");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("person_ID")+" " + resultSet.getString("first_name")+" "+ resultSet.getString("last_name"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
+        trialDatabase trial = new trialDatabase();
+        trial.trialFunction();
+
         Scanner scanner = new Scanner(System.in);
         DataBase dataBase = new DataBase();
         System.out.println("Sign up");
