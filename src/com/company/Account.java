e674db49ef4f8094d121eae4bdbd622ef68be28c
Ahmed-ali-abd-elsalam <85034642+Ahmed-ali-abd-elsalam@users.noreply.github.com>
2022-06-30 17:49:16 +0200

    removed unnessecary sql cod

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -105,11 +105,11 @@ public class Account {
     }
 
     public void AddMoney(double amount){
-        /*if(this.VerifyAccount()){
+        if(this.VerifyAccount()){
             this.setCash(this.getCash()+amount);
-        }*/
+        }
         //this sets the balance of the account or user
-        String update = "update person set balance = balance +"+amount+"where username = username";
+        /*String update = "update person set balance = balance +"+amount+"where username = username";
         try {
             // url is jdbc:mysql://localhost:3306/{database name}   user is the user of the database on the machine password should be the same as well
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketplace", "root", "Root_password");
@@ -117,10 +117,10 @@ public class Account {
             statement.executeUpdate(update);
         }catch (Exception e){
             e.printStackTrace();
-        }
-        /*else {
-            System.out.println("Incorrect data");
         }*/
+        else {
+            System.out.println("Incorrect data");
+        }
     }
     public void PayMoney(double amount){
         String update = "update person set balance = balance -"+amount+"where username = username";
