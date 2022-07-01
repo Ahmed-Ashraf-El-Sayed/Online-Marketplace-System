2e83226ec960d629775a353dda82d56c15dcfdfa
Mostafa Mohamed Naguib <104514845+Mostafanageb@users.noreply.github.com>
2022-07-01 23:54:11 +0200

    Add files via upload

--- LoginController.java
+++ LoginController.java
@@ -1,13 +1,22 @@
 package application;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable{
+	private Stage stage;
+	 private Scene scene;
+	 private Parent root;
 	
 	@FXML
 	private Label myLabel;
@@ -24,4 +33,13 @@ public class LoginController implements Initializable{
 
 	}
 
+	
+	 
+	 public void gotomainwindow(ActionEvent event) throws IOException {
+	  root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	  scene = new Scene(root);
+	  stage.setScene(scene);
+	  stage.show();
+	 }
 }
