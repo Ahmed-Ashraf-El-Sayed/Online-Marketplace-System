4ad9011bf4f26676a5db4483d5f3616f0dd96f4e
Abdelrahman Bauomyi <abdo01820@gmail.com>
2022-07-02 03:17:25 +0200

    changed the cart

--- CartController.java
+++ CartController.java
@@ -1,5 +1,40 @@
 package application;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class CartController {
+public class CartController implements Initializable{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+    public void switch_To_proceed(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("OrderDone.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+
+
+        public  void deleteItems(ActionEvent e){
+
+    }
+    public void calc_Price(ActionEvent e){
+
+    }
 }
